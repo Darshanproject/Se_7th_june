885a92bcd5e5d50415b12bbc4935ca2c50384bcd
Darshanproject <gadadarshan6184@gmail.com>
2023-06-21 09:15:59 +0530

    Done

---
+++ C Language/Untitled1.cpp
@@ -0,0 +1,18 @@
+/*
+int  %d
+double %lf
+char %c,%s
+float %f
+*/
+#include<stdio.h>
+main(){
+	int num_1 = 10;
+	char name='a';
+	float num2=25.28;
+	double num3=56.89;
+	printf("Hello Wolrd \n");
+	printf("%d \n",num_1);
+	printf("%c \n",name);
+	printf("%f \n",num2);
+	printf("%lf \n",num3);
+}
